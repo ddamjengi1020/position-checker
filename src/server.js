cfcafc03eb47f56c62a5e16f8c5c6cc67f04d170
ddamjengi1020 <silluat11@gmail.com>
2020-10-08 15:37:52 +0900

    #5.1 [bug]contentSecurityPolicy > false  on helmet in server js // fixedgit log

--- src/server.js
+++ src/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 4000;
 
 app.set("views", path.join(__dirname + "/views/screens"));
 app.set("view engine", "pug");
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use("/static", express.static(path.join(__dirname + "/static")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
